fbc81957bc8591fe372bf6ea2101cc817699035d
Kanishk Dixit <Kanishk.Dixit@target.com>
2025-12-07 14:16:58 +0530

    chaimadam

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-const repoName = "chaiMadam"; // 👉 replace with your repo name
-
 const nextConfig = {
-  output: "export",
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-  
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -20,6 +13,6 @@ const nextConfig = {
       },
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
